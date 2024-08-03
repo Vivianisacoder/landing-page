1c4aa599b7e4f9292549ee6ff076fb0d5d7284b7
Okechukwu Vivian <viviancodes7@gmail.com>
2024-08-03 10:56:48 +0100

    made corrections

--- scripts.js
+++ scripts.js
@@ -1,10 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Page loaded!");
-    var getStartedButton = document.querySelector(".get-started-button");
-    if (getStartedButton) {
-        getStartedButton.addEventListener("click", function() {
-            console.log("Get Started button clicked!");
-        });
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Page loaded!");
+  var getStartedButton = document.querySelector(".get-started-button");
+  if (getStartedButton) {
+    getStartedButton.addEventListener("click", function () {
+      console.log("Get Started button clicked!");
+    });
+  }
 });
-
